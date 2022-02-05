1335e8dc21d52506423cb2ec20fd70cc05b18915
Ars8 <pogosov-91@mail.ru>
2022-02-05 18:10:55 +0300

    new data fix

--- src/components/EventCalendar/index.tsx
+++ src/components/EventCalendar/index.tsx
@@ -12,35 +12,52 @@ const EventCalendar: FC<EventCalendarProps> = ({ events }) => {
 
 	console.log(events)
 
-  const now = events[1]?.appointmentDate;
-
-  const nowP = Date.parse(now);
-
-  const date = Math.floor(nowP/1000/60/60/24);
-
-  
+  /* const now = events[1]?.appointmentDate; */
+/* 	let dateA = events.forEach(function(item) {
+			const date = Math.floor((Date.parse(item.appointmentDate))/1000/60/60/24);
+			return date;
+		});
+		console.log(dateA) */
+		  
 
 	function getListData(value: any) {
+		/* const date = 19020; */
     const now = value.unix();
     const nowDate = Math.floor(now/60/60/24);
+		console.log(nowDate)                           //...19063
 		let listData;
-		switch (nowDate) {
-			case date:
+		listData = events.forEach((item) => (
+			console.log({type: item.appointmentDate, content: item.appointmentTime})
+		));
+
+		console.log(listData)
+
+
+
+
+		/* switch (nowDate) {
+			case 19020:
 				listData = events.map((event: any) => (
 					{type: "success", content: event.appointmentTime}
 				));
 				break;
+			case 19019:
+			listData = events.map((event: any) => (
+				{type: "success", content: event.appointmentTime}
+			));
+			break;
 			default:
-		}
+		} */
 		return listData || [];
 	}
 
 	function dateCellRender(value: any) {
 		const listData = getListData(value);
+		console.log(listData)
 		return (
 			<ul className="events">
-				{listData.map((item) => (
-					<li key={item.content}>
+				{listData.map((item, index) => (
+					<li key={index}>
 						<Badge status={"success"} text={item.content} />
 					</li>
 				))}
