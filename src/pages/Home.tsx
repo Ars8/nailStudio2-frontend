7bcdd09983133ed4bb85251dcf6f23779ff7d747
Ars8 <pogosov-91@mail.ru>
2022-05-20 09:12:51 +0300

    test at work2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,7 +13,7 @@ const Home: React.FC = () => {
 					className="site-layout-background"
 					style={{ padding: 24, minHeight: '100vh' }}
 				>
-					Content2
+					Content3
 				</div>
 			</Row>
 		</Layout.Content>
